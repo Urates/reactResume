d8a9a3bb0840e2f8224c6bab936cf7a35d96508d
Urates <umakiva@gmail.com>
2024-03-13 10:35:35 +0200

    responsiveness tested

--- src/components/Education.js
+++ src/components/Education.js
@@ -8,13 +8,13 @@ import Shortcourses from '../assets/shortcourses.jpg';
 function Education() {
     return (
       <div className="Education-container">
-        <p>Education</p>
-          <ul>
-            <li><img src={WSU} alt='Walter Sisulu University logo'/></li>
-            <li><img src={Wits} alt='Wits logo'/></li>
-            <li><img src={Fullstack} alt='Full Stack software Dev icon'/></li>
-            <li><img src={Shortcourses} alt='Shortcourses Icon'/></li>
-          </ul>
+        <p className='p'>Education</p>
+          <div className='education-items'>
+            <span><img src={WSU} alt='Walter Sisulu University logo'/><p>Walter Sisulu University</p></span>
+            <img src={Wits} alt='Wits logo'/><p>Wits University</p>
+            <span><img src={Fullstack} alt='Full Stack software Dev icon'/><p>Software Development</p></span>
+            <span><img src={Shortcourses} alt='Shortcourses Icon'/><p>Shortcourses</p></span>
+          </div>
       </div>
     );
   }
