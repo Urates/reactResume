c6ba1d0aaff7aa0e130522bf222514e03c5d1bde
Urates <umakiva@gmail.com>
2024-03-14 06:42:22 +0200

    background component updated once more

--- src/components/Background.js
+++ src/components/Background.js
@@ -21,11 +21,11 @@ function Background(){
                 <a href='#' id='self-sufficient' onClick={() => handleLinkClick('self-sufficient')}>Self-sufficient</a>
             </div>
             <div className='content-body'>
-              {selectedLink === 'efficient' && <p>I am always drawn to the idea of finishing much with less effort. Every skill I acquire is stamped efficiency in it; if I cannot do it efficiently, I simply do not know it.</p>}
-              {selectedLink === 'professional' && <p>Professional content here...</p>}
-              {selectedLink === 'tech-enabled' && <p>Tech-enabled content here...</p>}
-              {selectedLink === 'selfless' && <p>Selfless content here...</p>}
-              {selectedLink === 'self-sufficient' && <p>Self-sufficient content here...</p>}
+              {selectedLink === 'efficient' && <p>I am always drawn to the idea of achieving <b>much</b> with <b>less</b> effort. Every skill I acquire is stamped efficiency in it; if I cannot do it efficiently, I simply do not know it. In a world that has become so dependent on time it becomes extremely important to ensure that everything you do you mark TIME as the highest ghost worthy of all human respect.</p>}
+              {selectedLink === 'professional' && <p>We coexist in this world; so much that the existence of the other needs to be structured so that it peacefully synchronises with the whole human system. This calls for the development of a standard to in the cohabitate the world and that standard is none other than professionalism which encourages respect for everyone.</p>}
+              {selectedLink === 'tech-enabled' && <p>Tech is mostly, if not entirely, about efficiency and effectiveness. The initial goal of tech is to provide alternative and easier ways to lead our lives on this land. Most of my life is lead by technological operations in one way or another.</p>}
+              {selectedLink === 'selfless' && <p>As we navigate our way in this life it becomes complicated to choose with certainty the right way of living. For example, most people believe that you need to be self-centered to survive well in this world, I just chose being selfless and I am still happy with my life at least at a moral level.</p>}
+              {selectedLink === 'self-sufficient' && <p>Self-sufficiency in this context is used to mean that I generally do not require anyone behind me monitoring my learning because I have made learning as part of my daily meal. I no longer push myself to learn, I am actually trying to reduce the intensity of over-reading to save some time for a variety of other healthy habits.</p>}
             </div>
         </div>
       </div>
